f71d7b81b27d1a36b7c169a4d1e8c792da4a85ff
Ahmed Esmail <ahmed.esmail.fcis.2019@gmail.com>
2022-01-13 15:42:01 +0200

    Calculate Cumulative Distance + Present it

--- src/Components/TripDetailsModal/TripDetailsModal.js
+++ src/Components/TripDetailsModal/TripDetailsModal.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Alert, Modal, Text, View } from "react-native";
 import { RoundButton } from "..";
-import { secondsConverter } from "../../Helper/HelperFunction";
+import { getDistanceBtTwoCoordinate, secondsConverter } from "../../Helper/HelperFunction";
 import { navigate } from "../../Navigation/RootNavigation";
 import styles from "./styles";
 
 export function TripDetailsModal({ modalVisible, setModalVisible, liveLat, liveLong }) {
   const [pause, setPause] = useState(false);
-  const [counterTime, setCounterTime] = useState(5);
+  const [counterTime, setCounterTime] = useState(1);
   const [saveDate, setSaveDate] = useState(null);
+  const [pathData, setPathData] = useState([]);
+  const [cumulativeDistance, setCumulativeDistance] = useState(0);
   useEffect(() => {
     if (modalVisible) {
       onStartTrip();
@@ -20,9 +22,50 @@ export function TripDetailsModal({ modalVisible, setModalVisible, liveLat, liveL
         setCounterTime((c) => c + 1);
       }, 1000);
     }
+    if (pause) {
+      pathData.push({
+        lat: liveLat,
+        long: liveLong,
+        pause: true,
+      });
+    }
   }, [counterTime, pause]);
+  useEffect(() => {
+    if (modalVisible && !pause) {
+      pushNewPath();
+    }
+  }, [liveLat, liveLong]);
+  const pushNewPath = () => {
+    const { length } = pathData;
+    const previousPathItem = pathData[length - 1];
+    if (!previousPathItem?.pause) {
+      setCumulativeDistance(
+        (d) =>
+          d +
+          getDistanceBtTwoCoordinate(
+            liveLat,
+            liveLong,
+            previousPathItem?.lat,
+            previousPathItem?.long,
+          ),
+      );
+    }
+    pathData.push({
+      lat: liveLat,
+      long: liveLong,
+      pause,
+    });
+  };
   const onStartTrip = () => {
     setCounterTime(0);
+    setCumulativeDistance(0);
+    setPathData([
+      {
+        lat: liveLat,
+        long: liveLong,
+        pause: false,
+      },
+    ]);
     setSaveDate(new Date());
   };
   const onPausePress = () => {
@@ -34,6 +77,7 @@ export function TripDetailsModal({ modalVisible, setModalVisible, liveLat, liveL
   const onStopPress = () => {
     setModalVisible(false);
     setPause(false);
+    setPathData([]);
     navigate("HistoryScreen");
   };
   return (
@@ -49,12 +93,20 @@ export function TripDetailsModal({ modalVisible, setModalVisible, liveLat, liveL
         <Text style={styles.txt}>{secondsConverter(counterTime)}</Text>
         <View style={styles.rowBtThreeDetails}>
           <View style={styles.rowBtLabelResult}>
-            <Text style={styles.label}>Steps : </Text>
-            <Text style={styles.result}>{0}</Text>
+            <Text numberOfLines={1} style={styles.label}>
+              Distance :
+            </Text>
+            <Text numberOfLines={1} style={styles.result}>
+              {cumulativeDistance?.toFixed(2)} m
+            </Text>
           </View>
           <View style={styles.rowBtLabelResult}>
-            <Text style={styles.label}>Distance : </Text>
-            <Text style={styles.result}>{0} m</Text>
+            <Text numberOfLines={1} style={styles.label}>
+              Steps :
+            </Text>
+            <Text numberOfLines={1} style={styles.result}>
+              {0}
+            </Text>
           </View>
         </View>
         <View style={styles.rowBtBttns}>
--- src/Helper/HelperFunction.js
+++ src/Helper/HelperFunction.js
@@ -39,3 +39,30 @@ export const secondsConverter = (numberOfSeconds) => {
     seconds < 10 ? 0 : ""
   }${seconds}`;
 };
+
+export const calculateDistance = (numberOfSeconds) => {
+  const hours = Math.floor(numberOfSeconds / 3600);
+  let rest = numberOfSeconds % 3600;
+  const minutes = Math.floor(rest / 60);
+  rest %= 60;
+  const seconds = rest;
+  return `${hours < 10 ? 0 : ""}${hours}:${minutes < 10 ? 0 : ""}${minutes}:${
+    seconds < 10 ? 0 : ""
+  }${seconds}`;
+};
+
+export const getDistanceBtTwoCoordinate = (lat1, lon1, lat2, lon2) => {
+  const radLat1 = (Math.PI * lat1) / 180;
+  const radLat2 = (Math.PI * lat2) / 180;
+  const theta = lon1 - lon2;
+  const radTheta = (Math.PI * theta) / 180;
+  let dist =
+    Math.sin(radLat1) * Math.sin(radLat2) +
+    Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+  dist = Math.acos(dist);
+  dist = (dist * 180) / Math.PI;
+  dist = dist * 60 * 1.1515;
+  dist *= 1.609344;
+
+  return dist * 1000;
+};
